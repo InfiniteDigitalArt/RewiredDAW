e50321eed2b965425f5b112b012cc312f6ef7e40
InfiniteDigitalArt <69279920+InfiniteDigitalArt@users.noreply.github.com>
2026-01-11 21:56:59 +0000

    Delete daw directory

--- daw/assets/loops.js
+++
@@ -1,9 +0,0 @@
-window.DROPBOX_LOOPS = [
-  "https://dl.dropboxusercontent.com/scl/fi/ykylhpxuppypbjzepw7eg/RRML1_Open_Hihat_14_175bpm.wav?rlkey=7xwbv3owdlcnzkgycyybpz32u&st=wyjenjve",
-  "https://dl.dropboxusercontent.com/scl/fi/6nboux0mg4pcvqi7f1ibx/RRML1_Kick_12_175bpm.wav?rlkey=y3nf0jfj2vjukesgqdwvxy1fz&st=kbvoq8lr",
-  "https://dl.dropboxusercontent.com/scl/fi/h3liwc8zmwilyri646g4a/RRML1_Acid_7_F-_175bpm.wav?rlkey=h9xkha9ruv5wid4pmwtqlismv&st=syaddaiu",
-  "https://dl.dropboxusercontent.com/scl/fi/x42db2poyazafi8ipkm1p/RRML1-Crash-5.wav?rlkey=l1d7y5zkbk73dbtaux1bg26cn",
-  "https://dl.dropboxusercontent.com/scl/fi/d171mgn1eq0fqwd0rqel7/RRML1_Rayada_8_F-_175bpm.wav?rlkey=zid0stprfvctbfiaox3gdzbbe",
-  "https://dl.dropboxusercontent.com/scl/fi/eb5s6a4tt0hpo1oc1tkg0/Lately-Bass-F-175bpm.wav?rlkey=pg8vu3be64zy58kuglxj69t8x&st=jxc5d0fg",
-
-];
